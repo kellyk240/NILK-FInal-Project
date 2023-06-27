cd2d80ffabd3a3026cc57fc791b91e4af3d52946
Kelly Kim <kellykim240@gmail.com>
2023-06-27 09:35:01 -0400

    Initial Commit

---
+++ NILK FInal Project/NILK_FInal_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NILK_FInal_ProjectApp.swift
+//  NILK FInal Project
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct NILK_FInal_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
